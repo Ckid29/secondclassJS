4bed538a0eddda518237edfcd70592b6f8313aaf
Nathan Hundley <100338130+Ckid29@users.noreply.github.com>
2022-04-06 18:46:46 -0500

    SecondclassJS
    SecondclassJS

---
+++ script.js
@@ -0,0 +1,15 @@
+// keyword name = value;
+//let userName = prompt("Enter your Name: ");
+//document.write("Welcome " + userName);
+//const taxes=0.11;
+//taxes=0.08;
+let userName = "Aldrian"; //string
+console.log(userName);
+userName=true; //boolean
+userName=99; //number
+console.log(userName);
+
+let myArray =["string",10,{}];
+let theSameArray = new Array(1, 2, 3);
+
+console.log(myArray[2]); //10
